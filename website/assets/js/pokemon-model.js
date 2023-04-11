24abf52c4d447434d2dea58b6acd45f8c2ed3903
Douglas Silveira <douglas81silveira@gmail.com>
2023-04-11 15:17:09 -0300

    Criação do arquivo de configuração docker-compose e copiado os arquivos da aplicação de teste

---
+++ website/assets/js/pokemon-model.js
@@ -0,0 +1,19 @@
+class Pokemon {
+  number;
+  name;
+  type;
+  types = [];
+  photo;
+
+  hp;
+  attack;
+  defense;
+  specialAttack;
+  specialDefense;
+  speed;
+
+  abilities = [];
+
+  height;
+  weight;
+}
